160dbbbef9c42ad3263fcfa2851ddd1ff167c0ac
WolfPark <wolfparktaerim@gmail.com>
2025-05-09 22:04:23 +0800

    removed some useless files; installed PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,52 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import { VitePWA } from 'vite-plugin-pwa'
+
+
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5MB
+      },
+      includeAssets: [
+        'favicon.svg',
+        'robots.txt',
+        'apple-touch-icon.png'
+      ],
+      manifest: {
+        name: 'Muneh Thracker',
+        short_name: 'ExpenseApp',
+        description: 'Track your expenses easily!',
+        theme_color: '#42b883',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
